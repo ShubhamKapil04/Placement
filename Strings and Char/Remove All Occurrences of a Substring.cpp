dc0609782549a80765f85fd4dd43325be2816fba
Shubham Kapil <76792717+Skaplesh04@users.noreply.github.com>
2022-03-16 22:02:03 +0530

    Find and delete

---
+++ Strings and Char/Remove All Occurrences of a Substring.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{   
+    string s;
+    getline (cin, s);
+    string part;
+    getline(cin, part);
+
+    while(s.length() != 0 && s.find(part) < s.length())
+    {
+        s.erase(s.find(part), part.length());
+    }
+    
+    cout << s;
+    return 0;
+}
