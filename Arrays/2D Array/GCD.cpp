703ed055d3c76735fa19204df0e46a6900431c45
Shubham Kapil <76792717+Skaplesh04@users.noreply.github.com>
2022-03-20 23:22:49 +0530

    GCD

---
+++ Arrays/2D Array/GCD.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int gcd(int a, int b)
+{
+    if(a==0)
+    {
+        return b;
+    }
+    if(b==0)
+    {
+        return a;
+    }
+
+    while(a!=b)
+    {
+        if(a>b)
+        {
+            a = a - b;
+        }
+        else
+        {
+            b = b - a;
+        }
+    }
+    return a;
+}
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    int ans = gcd (a,b);
+
+    cout << "GCD of " << a << " & " << b << " is " << ans << endl;
+
+    return 0;
+}
