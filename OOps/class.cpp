a4a83ddd3b69c46b3eaf97cf278f39ae10a3475d
Shubham Kapil <Skaplesh720@gmail.com>
2022-04-14 17:10:58 +0530

    OOPS

---
+++ OOps/class.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+//Creating Class
+
+class Hero{
+
+    //Properties of Class
+    public:
+    char Name[100];
+
+
+
+
+};
+int main()
+{
+    //Function of class
+
+    Hero h1;
+    h1.Name = "Shubham";
+
+    cout<< h1.Name;
+
+
+    return 0;
+}
