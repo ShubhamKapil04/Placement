5ae8978240047a55e3fa9951f5d08960357d2e3c
Shubham Kapil <Skaplesh720@gmail.com>
2022-04-16 01:36:06 +0530

    Public Modifier and Protected Modifier

--- OOps/AccessModifiers.cpp
+++ OOps/AccessModifiers.cpp
@@ -5,10 +5,9 @@ using namespace std;
 class Car{
     //By Default Private Access Modifier Works
     //We won't initialize it
-    int topSpeed;
-
-    //Using Public Modifier
     public:
+    int topSpeed;
+    //Using Public Modifier
     string CarName;
 
     protected: 
@@ -20,10 +19,22 @@ int main()
 {
     //Creating Object of class
     // statical object allocation
-    Car.car1;
+    Car car1;
+    car1.CarName = "Audi";
+    car1.topSpeed = 100;
+
+    cout<< " The name of car 1 is--> " << car1.CarName <<endl;
+    cout<< " The topSpeed of car is --> " << car1.topSpeed <<endl;
 
     //Dynamicaly object allocation
     Car *car2 = new Car();
+   // car2->CarName = "BMW";
+   // car2->topSpeed = 200;
+
+    //cout<< " The name of car 1 is--> " << car2.CarName <<endl;
+   // cout<< " The topSpeed of car is --> " << car2.topSpeed <<endl;
+
+
     //This is How we create Dynamic Memory for object
 
 }
