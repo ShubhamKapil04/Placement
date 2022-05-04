8c1b92b299c1cd4f3818f6413e0d5e8481e246d4
Shubham Kapil <Skaplesh720@gmail.com>
2022-05-04 23:42:49 +0530

    Array

--- Linked List/Questions/tempCodeRunnerFile.cpp
+++ Linked List/Questions/tempCodeRunnerFile.cpp
@@ -1,1 +1,7 @@
- tail->next = head->next;
+
+    insertionAtHead(head, 32);
+    insertionAtHead(head, 34);
+    insertionAtHead(head, 36);
+    insertionAtHead(head, 37);
+    insertionAtHead(head, 38);
+    insertionAtHead(head, 39);
