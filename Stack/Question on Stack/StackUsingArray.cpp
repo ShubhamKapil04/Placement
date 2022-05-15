39a7498ab9c32a1a1992bf5e345ca5295d780ff0
Shubham Kapil <Skaplesh720@gmail.com>
2022-05-15 17:47:41 +0530

    Stack using arrays

---
+++ Stack/Question on Stack/StackUsingArray.cpp
@@ -0,0 +1,110 @@
+/*
+Implement stack using array 
+Basic Accuracy: 50.0% Submissions: 74764 Points: 1
+Write a program to implement a Stack using Array. Your task is to use the class as shown in the comments in the code editor and complete the functions push() and pop() to implement a stack. 
+
+Example 1:
+
+Input: 
+push(2)
+push(3)
+pop()
+push(4) 
+pop()
+Output: 3, 4
+Explanation: 
+push(2)    the stack will be {2}
+push(3)    the stack will be {2 3}
+pop()      poped element will be 3,
+           the stack will be {2}
+push(4)    the stack will be {2 4}
+pop()      poped element will be 4
+Example 2:
+
+Input: 
+pop()
+push(4)
+push(5)
+pop()
+Output: -1, 5
+Your Task:
+You are required to complete two methods push() and pop(). The push() method takes one argument, an integer 'x' to be pushed into the stack and pop() which returns an integer present at the top and popped out from the stack. If the stack is empty then return -1 from the pop() method.
+
+Expected Time Complexity : O(1) for both push() and pop().
+Expected Auixilliary Space : O(1) for both push() and pop().
+
+Constraints:
+1 <= Q <= 100
+1 <= x <= 100
+*/
+
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+class MyStack
+{
+private:
+    int arr[1000];
+    int top;
+public:
+    MyStack(){top=-1;}
+    int pop();
+    void push(int);
+};
+
+
+int main()
+{
+
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        MyStack *sq = new MyStack();
+
+        int Q;
+        cin>>Q;
+        while(Q--){
+        int QueryType=0;
+        cin>>QueryType;
+        if(QueryType==1)
+        {
+            int a;
+            cin>>a;
+            sq->push(a);
+        }else if(QueryType==2){
+            cout<<sq->pop()<<" ";
+
+        }
+        }
+        cout<<endl;
+    }
+}
+// } Driver Code Ends
+
+
+
+//Function to push an integer into the stack.
+void MyStack :: push(int x)
+{
+    if(top>1000){
+        return;
+    }
+    else{
+        arr[top] = x;
+        top++;
+    }
+}
+
+//Function to remove an item from top of the stack.
+int MyStack :: pop()
+{
+    if(top == -1){
+        return -1;
+    }   
+    else{
+       top--;
+       return arr[top];
+    }
+}
