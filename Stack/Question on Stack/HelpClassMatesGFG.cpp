3f69d9234b77b78cafd0d2c41302805cdef06b78
Shubham Kapil <Skaplesh720@gmail.com>
2022-05-18 15:13:48 +0530

    Help ClassMates Or Next Smallest Element

---
+++ Stack/Question on Stack/HelpClassMatesGFG.cpp
@@ -0,0 +1,64 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    
+    public:
+    vector<int> help_classmate(vector<int> arr, int n) 
+    { 
+    stack<int> s;
+    s.push(-1);
+    vector<int> ans(n);
+    
+    for(int i=n-1; i>=0 ; i--) {
+        int curr = arr[i];
+        while(s.top() >= curr)
+        {
+            s.pop();
+        }
+        //ans is stack ka top
+        ans[i] = s.top();
+        s.push(curr);
+    }
+    // for(int i = 0 ; i < ans.size(); i++){
+    //     cout << ans[i];
+    // }
+    return ans;
+    } 
+};
+
+// { Driver Code Starts.
+
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		vector<int> array(n);
+		for (int i = 0; i < n; ++i)
+		{
+			cin>>array[i];
+		}
+		Solution obj;
+		vector<int> result = obj.help_classmate(array,n);
+		for (int i = 0; i < n; ++i)
+		{
+			cout<<result[i]<<" ";
+		}
+		cout<<"\n";
+        
+	}
+	return 0; 
+}
+
+  // } Driver Code Ends
