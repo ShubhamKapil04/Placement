4c28a7996dddaeef57398292326676c925981bd1
Shubham Kapil <Skaplesh720@gmail.com>
2022-05-25 20:01:22 +0530

    Circular Queue

---
+++ Queue/Circular Queue/CircularQueue.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+class CircularQueue{
+    public:
+    int *arr;
+    int front;
+    int rare;
+    int size;
+
+    // Intializing the Queue
+    CircularQueue(){
+        arr = new int[size];
+        front = rare = -1;
+    }
+
+    //
+};
+
+int main()
+{
+
+}
