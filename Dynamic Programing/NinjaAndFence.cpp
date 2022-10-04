cdff93cf6a4dc50a0359c78cdf468cbb1964bad4
Shubham Kapil <skaplesh720@gmail.com>
2022-10-05 00:17:27 +0530

    Ninja Fence

---
+++ Dynamic Programing/NinjaAndFence.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+#define MOD 1000000007
+
+int add(int a, int b){
+    return ((a % MOD + b % MOD) % MOD);
+}
+
+int mul(int a, int b){
+    return ((a % MOD * (b % MOD))% MOD);
+}
+
+int numberofWay(int n, int k){
+
+    if(n == 1) return k;
+    if(n == 2) return add(k, mul(k, k-1));
+
+    int ans = add(mul(numberofWay(n-2, k), k-1), mul(numberofWay(n-1, k), k-1));
+
+    return ans;
+}
+
+int main(){
+
+    int n = 3;
+    int k = 2;
+
+    int ans = numberofWay(n, k);
+    cout << ans;
+
+    return 0;
+}
